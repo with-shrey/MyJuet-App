f20795560afe10ef5010bc799a466299cfccdf44
ShreyGupta <shrey.gupta1nov@gmail.com>
2017-03-08 19:09:50 +0530

    post and get working fine code refactored

--- app/src/main/java/app/myjuet/com/myjuet/AttendenceActivity.java
+++ app/src/main/java/app/myjuet/com/myjuet/AttendenceActivity.java
@@ -5,16 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.HttpURLConnection;
-import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
+import app.myjuet.com.myjuet.web.webUtilities;
 
 public class AttendenceActivity extends AppCompatActivity {
 
@@ -32,8 +29,7 @@ public class AttendenceActivity extends AppCompatActivity {
     }
 
     private class extractor extends AsyncTask<Void,Void,String>{
-        HttpURLConnection conn=null;
-        private final String USER_AGENT = "Mozilla/5.0";
+
         @Override
         protected String doInBackground(Void... voids) {
 
@@ -41,14 +37,13 @@ public class AttendenceActivity extends AppCompatActivity {
             String PostParam="txtInst=Institute&InstCode=JUET&txtUType=Member+Type&UserType=S&txtCode=Enrollment No&MemberCode=161B222&txtPIN=Password%2FPin&Password=jaishriram&BTNSubmit=Submit";
         String Attendence="https://webkiosk.juet.ac.in/StudentFiles/Academic/StudentAttendanceList.jsp";
             String Content="";
-            extractor http = new extractor();
-
             try {
-                http.sendPost(Url,PostParam);
-                Content=http.GetPageContent(Attendence);
+                webUtilities.sendPost(Url, PostParam);
+                Content = webUtilities.GetPageContent(Attendence);
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
             return Content;
 
         }
@@ -59,56 +54,6 @@ public class AttendenceActivity extends AppCompatActivity {
             text.setText(s);
             super.onPostExecute(s);
         }
-        private void sendPost(String url, String postParams) throws Exception {
-            URL obj = new URL(url);
 
-            conn = (HttpsURLConnection) obj.openConnection();
-            conn.setUseCaches(false);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Host", "webkiosk.juet.ac.in");
-            conn.setRequestProperty("User-Agent", USER_AGENT);
-            conn.setRequestProperty("Accept",
-                    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-            conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-            conn.setRequestProperty("Connection", "close");
-            conn.setRequestProperty("Referer", "webkiosk.juet.ac.in");
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestProperty("Content-Length", Integer.toString(postParams.length()));
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-            wr.writeBytes(postParams);
-            wr.flush();
-            wr.close();
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-        }
-
-
-        private String GetPageContent(String url) throws Exception {
-            URL obj = new URL(url);
-            conn = (HttpsURLConnection) obj.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setUseCaches(false);
-            conn.setRequestProperty("User-Agent", USER_AGENT);
-            conn.setRequestProperty("Accept",
-                    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-            conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-            int responseCode = conn.getResponseCode();
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            return response.toString();
-        }
     }
 }
--- app/src/main/java/app/myjuet/com/myjuet/web/webUtilities.java
+++ app/src/main/java/app/myjuet/com/myjuet/web/webUtilities.java
@@ -1,8 +1,88 @@
 package app.myjuet.com.myjuet.web;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.R.id.input;
+
 /**
  * Created by Shrey on 08-Mar-17.
  */
 
 public class webUtilities {
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static HttpURLConnection conn = null;
+
+    // Creating url and catching exception
+    private static URL UrlCreator(String url) {
+        URL link = null;
+        try {
+            link = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return link;
+    }
+
+    //sending Post to a link with paramaters
+    public static void sendPost(String url, String postParams) throws Exception {
+        URL obj = UrlCreator(url);
+
+        conn = (HttpsURLConnection) obj.openConnection();
+        conn.setUseCaches(false);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Host", "webkiosk.juet.ac.in");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        conn.setRequestProperty("Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        conn.setRequestProperty("Connection", "close");
+        conn.setRequestProperty("Referer", "webkiosk.juet.ac.in");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", Integer.toString(postParams.length()));
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+        wr.writeBytes(postParams);
+        wr.flush();
+        wr.close();
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+    }
+
+
+    public static String GetPageContent(String url) throws Exception {
+        URL obj = UrlCreator(url);
+        String Result = null;
+        conn = (HttpsURLConnection) obj.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setUseCaches(false);
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        conn.setRequestProperty("Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        int responseCode = conn.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
 }
